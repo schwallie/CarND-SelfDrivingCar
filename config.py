Took out 0 throttles, fixed normalization

config.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import pandas as pd
 from keras.optimizers import Adam
 
 IMAGE_HEIGHT_CROP = 108
@@ -7,8 +8,8 @@ IMAGE_WIDTH_CROP = 320
 IMAGE_HEIGHT = 64
 IMAGE_WIDTH = 64
 # (200, 66) <-- Original NVIDIA Paper
-IMAGE_WIDTH = 200
-IMAGE_HEIGHT = 66
+# IMAGE_WIDTH = 200
+# IMAGE_HEIGHT = 66
 LR = 1e-4
 OPTIMIZER = Adam(lr=LR)
 LOSS = 'mse'
@@ -27,3 +28,14 @@ def return_image(img):
     # img = np.array(img)/255. - 0.5
     img = (cv2.resize(img, (IMAGE_WIDTH, IMAGE_HEIGHT), interpolation=cv2.INTER_AREA))
     return np.float32(img)
+
+def flip_image():
+    pass
+
+def add_flipped_images(path = 'data/altered_drive_df.csv'):
+    drive_df = pd.read_csv(path)
+    for idx, row in drive_df.iterrows():
+        rnd = np.random.randint(2)
+        if rnd == 1:
+            # Flip and created a new image
+            pass

model.py
@@ -1,19 +1,22 @@
+import math
+
 import cv2
 import numpy as np
 from keras.callbacks import ModelCheckpoint
 from keras.layers import Convolution2D, ELU
 from keras.layers import Dense, Flatten
-from keras.layers.core import Lambda
+from keras.layers.core import Lambda, Dropout
 from keras.models import Sequential
 
 import config
 import load_data
 
+
 def steering_net():
-    # p = .5
+    p = .5
     model = Sequential()
     # Vivek, color space conversion layer so the model automatically figures out the best color space
-    model.add(Lambda(lambda x: x / 127.5 - .5,
+    model.add(Lambda(lambda x: x / 255. - .5,
                      input_shape=(config.IMAGE_HEIGHT, config.IMAGE_WIDTH, 3)))
     model.add(Convolution2D(3, 1, 1, border_mode='same', name='color_conv'))
     model.add(Convolution2D(24, 5, 5, init='he_normal', subsample=(2, 2), name='conv1'))
@@ -35,12 +38,10 @@ def steering_net():
     # model.add(Dropout(p))
     model.add(Dense(50, init='he_normal', name="dense_50"))  #
     model.add(ELU())
-    # model.add(Dropout(p))
+    model.add(Dropout(p))
     model.add(Dense(10, init='he_normal', name="dense_10"))  #
     model.add(ELU())
     model.add(Dense(1, init='he_normal', name="dense_1"))  #
-    # model.add(Lambda(atan_layer, output_shape=atan_layer_shape, name="atan_0"))
-
     return model
 
 
@@ -52,7 +53,7 @@ def get_model():
 
 def generate_arrays(X_train, y_train):
     while 1:
-        for ix in range(int(len(X_train) / config.BATCH_SIZE)):
+        for ix in range(math.floor(len(X_train) / config.BATCH_SIZE)):
             paths = X_train[ix * config.BATCH_SIZE:(ix + 1) * config.BATCH_SIZE]
             imgs = [config.return_image(cv2.imread(f)) for f in paths]
             yield np.array(imgs), np.array(y_train[ix * config.BATCH_SIZE:(ix + 1) * config.BATCH_SIZE])
@@ -73,7 +74,7 @@ def train(data=None):
     checkpoint = ModelCheckpoint(checkpoint_path, verbose=1, save_best_only=False, save_weights_only=False, mode='auto')
     model.fit_generator(generate_arrays(X_train, y_train),
                         validation_data=(np.asarray(X_validate), np.asarray(y_validate)),
-                        samples_per_epoch=len(X_validate)*5,
+                        samples_per_epoch=len(X_validate) * 5,
                         nb_epoch=config.NB_EPOCH, verbose=1, callbacks=[checkpoint])
 
 
@@ -89,5 +90,3 @@ def save_model(model):
     json_string = model.to_json()
     model.save_weights('model.h5')
     json.dump(json_string, open('model.json', 'w'))
-
-