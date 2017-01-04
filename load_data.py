820e7adc4d533b2d3184e0a2d64c486e43d04351
Chase Schwalbach <chase.schwalbach@avant.com>
2017-01-04 11:33:13 -0600

    New Updates, took out L/R again to see what center looks like

--- load_data.py
+++ load_data.py
@@ -6,12 +6,12 @@ from sklearn.model_selection import train_test_split
 def load_data(path='data/driving_log.csv'):  # altered_driving_log.csv
     drive_df = pd.read_csv(path)
     # TODO: Try .08 instead of .25
-    drive_df['left_steering'] = drive_df['steering'] + .08
-    drive_df['right_steering'] = drive_df['steering'] - .08
+    # drive_df['left_steering'] = drive_df['steering'] + .08
+    # drive_df['right_steering'] = drive_df['steering'] - .08
     drive_df = drive_df.rename(columns={'steering': 'center_steering'})
     X_data = []
     y_data = []
-    for cam_type in ['center', 'left', 'right']:
+    for cam_type in ['center']: # , 'left', 'right']:
         drive_df[cam_type] = drive_df[cam_type].str.strip()
         vals = drive_df[cam_type].values
         arr = ['data/{0}'.format(f) for f in vals]
