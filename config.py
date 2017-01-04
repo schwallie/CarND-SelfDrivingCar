3c7b41648f1e8b640cd22106491b78ea0bdf81c5
Chase Schwalbach <chase.schwalbach@avant.com>
2017-01-04 16:13:10 -0600

    New model attempt

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ IMAGE_WIDTH_CROP = 320
 THROTTLE_ADJUSTMENT = 1
 AUTONOMOUS_THROTTLE = .2
 # (200, 66) <-- Original NVIDIA Paper
-IMAGE_WIDTH = 200
-IMAGE_HEIGHT = 66
+IMAGE_WIDTH = 320
+IMAGE_HEIGHT = 160
 LR = 1e-4
 OPTIMIZER = Adam(lr=LR)
 LOSS = 'mse'
@@ -22,12 +22,12 @@ BATCH_SIZE = 128
 def return_image(img, color_change=True):
     # Take out the dash and horizon
     img_shape = img.shape
-    crop_img = img[int(img_shape[0] / 5):img_shape[0] - 20, 0:img_shape[1]]
-    assert crop_img.shape[0] == IMAGE_HEIGHT_CROP
-    assert crop_img.shape[1] == IMAGE_WIDTH_CROP
+    # crop_img = img[int(img_shape[0] / 5):img_shape[0] - 20, 0:img_shape[1]]
+    # assert crop_img.shape[0] == IMAGE_HEIGHT_CROP
+    # assert crop_img.shape[1] == IMAGE_WIDTH_CROP
     if color_change:
-        img = cv2.cvtColor(crop_img, cv2.COLOR_BGR2RGB)
-    img = (cv2.resize(img, (IMAGE_WIDTH, IMAGE_HEIGHT), interpolation=cv2.INTER_AREA))
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    # img = (cv2.resize(img, (IMAGE_WIDTH, IMAGE_HEIGHT), interpolation=cv2.INTER_AREA))
     return np.float32(img)
 
 
@@ -42,19 +42,18 @@ def add_flipped_images(path):
     addition = {}
     for idx, row in drive_df.iterrows():
         rnd = np.random.randint(2)
-        if rnd == 1:
-            maxidx += 1
-            # Flip and created a new image
-            img_path = 'data/{0}'.format(row['center'])
-            new_path = 'IMG/FLIPPED_{0}'.format(row['center'].split('/')[-1])
-            img = cv2.imread(img_path)
-            img = np.array(img)
-            img = cv2.flip(img, 1)
-            cv2.imwrite('data/{0}'.format(new_path), img)
-            steer = -row['steering']
-            addition[maxidx] = {'center': new_path, 'steering': steer}
+        # if rnd == 1:
+        maxidx += 1
+        # Flip and created a new image
+        img_path = 'data/{0}'.format(row['center'])
+        new_path = 'IMG/FLIPPED_{0}'.format(row['center'].split('/')[-1])
+        img = cv2.imread(img_path)
+        img = np.array(img)
+        img = cv2.flip(img, 1)
+        cv2.imwrite('data/{0}'.format(new_path), img)
+        steer = -row['steering']
+        addition[maxidx] = {'center': new_path, 'steering': steer}
     new_df = pd.DataFrame.from_dict(addition, orient='index')
-    print(new_df)
     drive_df = pd.concat([drive_df, new_df])
     drive_df.to_csv(path)
 
