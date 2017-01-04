117e4e4d924d94da1d3662dae2ca27c861f33646
Chase Schwalbach <chase.schwalbach@avant.com>
2017-01-04 09:55:45 -0600

    Trying .08 L/R alterations

--- load_data.py
+++ load_data.py
@@ -11,7 +11,7 @@ def load_data(path='data/driving_log.csv'):  # altered_driving_log.csv
     drive_df = drive_df.rename(columns={'steering': 'center_steering'})
     X_data = []
     y_data = []
-    for cam_type in ['center']:  # , 'left', 'right']:
+    for cam_type in ['center', 'left', 'right']:
         drive_df[cam_type] = drive_df[cam_type].str.strip()
         vals = drive_df[cam_type].values
         arr = ['data/{0}'.format(f) for f in vals]
