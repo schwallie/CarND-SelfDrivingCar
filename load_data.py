66ff53ead0a9f402bcfec0012920adbda7ad46cb
Chase Schwalbach <chase.schwalbach@avant.com>
2017-01-05 14:58:22 -0600

    bug fix

--- load_data.py
+++ load_data.py
@@ -19,7 +19,8 @@ def load_data(path='data/driving_log.csv'):  # altered_driving_log.csv
         vals = drive_df[pd.notnull(drive_df[cam_type])][cam_type].values
         arr = ['data/{0}'.format(f) for f in vals]
         X_data.extend(arr)
-        y_data.extend(drive_df['{0}_steering'.format(cam_type)].values)
+        y_cam_type = '{0}_steering'.format(cam_type)
+        y_data.extend(drive_df[pd.notnull(drive_df[y_cam_type])][y_cam_type].values)
     y_data = np.float32(y_data)
     # Shuffle since I'm not doing validation
     # TODO: Make sure I didn't screw up anything with shuffle
